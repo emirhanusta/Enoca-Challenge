58be66cf29b7c3d8b643ca4ac57ac6eb421c1ab0
emirhanusta <emirhan1usta@gmail.com>
2024-11-24 23:05:29 +0300

    initial commit

---
+++ src/main/java/Enoca_Challenge/EnocaChallengeApplication.java
@@ -0,0 +1,13 @@
+package Enoca_Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Enoca_Challenge/EnocaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package Enoca_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
